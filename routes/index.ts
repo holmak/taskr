b3cab73b8256ec9dd0289e36c1991d1e5a787cf3
Keith Holman <holmak@gmail.com>
2022-10-01 09:57:39 -0700

    New tasks can be created with a standard form.

--- routes/index.ts
+++ routes/index.ts
@@ -4,9 +4,16 @@ import Database from 'better-sqlite3';
 const router = express.Router();
 export default router;
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   const db = new Database('task.db');
   const stmt = db.prepare('SELECT title FROM tasks');
   const tasks = stmt.all();
   res.render('index', { title: 'taskr', tasks: tasks });
+});
+
+router.post('/create', function(req, res) {
+  const db = new Database('task.db');
+  const stmt = db.prepare('INSERT INTO tasks VALUES(?)');
+  stmt.run(req.body.title);
+  res.redirect('/');
 });
