58404b238fd5253436f383b58e39cfc60d78c1c3
Keith Holman <holmak@gmail.com>
2022-10-01 09:36:17 -0700

    Retrieve tasks from a SQLite database.

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import Database from 'better-sqlite3';
 
 const router = express.Router();
 export default router;
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const db = new Database('task.db');
+  const stmt = db.prepare('SELECT title FROM tasks');
+  const tasks = stmt.all();
+  res.render('index', { title: 'taskr', tasks: tasks });
 });
