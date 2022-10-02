676e925ba6d09c2b588bf5de0f6084a480099507
Keith Holman <holmak@gmail.com>
2022-10-01 18:15:09 -0700

    Tasks can be deleted, in bulk.

--- routes/index.ts
+++ routes/index.ts
@@ -6,14 +6,29 @@ export default router;
 
 router.get('/', function(req, res) {
   const db = new Database('task.db');
-  const stmt = db.prepare('SELECT title FROM tasks');
+  const stmt = db.prepare('SELECT rowid, title FROM tasks');
   const tasks = stmt.all();
   res.render('index', { title: 'taskr', tasks: tasks });
 });
 
 router.post('/create', function(req, res) {
   const db = new Database('task.db');
-  const stmt = db.prepare('INSERT INTO tasks VALUES(?)');
-  stmt.run(req.body.title);
+  const title = req.body.title;
+  if (title !== undefined && title !== '') {
+    const stmt = db.prepare('INSERT INTO tasks VALUES(?)');
+    stmt.run(title);
+  }
+  res.redirect('/');
+});
+
+router.post('/delete', function(req, res) {
+  const db = new Database('task.db');
+  const ids = req.body.id;
+  if (ids !== undefined) {
+    const stmt = db.prepare('DELETE FROM tasks WHERE rowid = ?');
+    for (const id of ids) {
+      stmt.run(id);
+    }
+  }
   res.redirect('/');
 });
